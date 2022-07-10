73bfdde564a16de6822dedce7bc88a36c5f372b9
Lose88 <hotsting1888@gmail.com>
2022-07-10 19:47:20 +0300

    add .gitignore

---
+++ src/main/java/ru/netology/api/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.api;
+
+public class Post {
+    public String surname;
+    public String name;
+    public String patronymic;
+    public String phone;
+    public String passport;
+    public FormDate birthday;
+    public boolean subscription;
+
+}
